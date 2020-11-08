12751bdbc6c982035899b6f773b8be80a2450160
ohughes16 <ohughes16@gmail.com>
2020-11-07 20:56:42 -0800

    Working through 9.5.4

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-engine = create_engine("sqlite://hawaii.sqlite")
+engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
-Base.prepare(engine, reflect = True)
-Measurement = Base.classes.Measurement
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
 Station = Base.classes.station
 session = Session(engine)
 
@@ -32,5 +32,11 @@ def precipitation():
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation = session.query(Measurement.date, Measurement.prcp).\
       filter(Measurement.date >= prev_year).all()
-      precip = {date: prcp for date, prcp in precipitation}
+    precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
